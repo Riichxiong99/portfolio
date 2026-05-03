e3b25e56461af5e1a1bb5f69193a23a8919f9898
rxa14 <rxa14@sfu.ca>
2026-05-02 18:29:01 -0700

    Added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer() {
+  return (
+    <footer>
+      <span>Richard Xiong © 2026</span>
+      <div className="footer-links">
+        <a href="https://github.com/rxa14" target="_blank" rel="noreferrer">GitHub</a>
+        <a href="https://linkedin.com/in/yourhandle" target="_blank" rel="noreferrer">LinkedIn</a>
+        <a href="mailto:richardxiong0224@hotmail.com">Email</a>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
