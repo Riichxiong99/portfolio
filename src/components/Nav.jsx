b61f8e3fc8d0fa13f6ea833ae1a63223bc0504d4
Riichxiong99 <richardxiong0224@hotmail.ca>
2026-05-02 18:31:49 -0700

    Added navigation bar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,21 @@
+function Nav() {
+  const scrollToTop = (e) => {
+    e.preventDefault()
+    document.documentElement.scrollTo({ top: 0, behavior: 'smooth' })
+    document.body.scrollTo({ top: 0, behavior: 'smooth' })
+  }
+
+  return (
+    <nav>
+      <a href="#" onClick={scrollToTop}>R.X.</a>
+      <ul>
+        <li><a href="#about">About</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#work">Work</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav
