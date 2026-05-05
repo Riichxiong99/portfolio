dab52d6452d2a8038659ba77845d82b3a180c1d9
Riichxiong99 <richardxiong0224@hotmail.ca>
2026-05-04 23:07:37 -0700

    Updated About me

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,7 +2,9 @@ function About() {
   return (
     <div id="about" className="fade-section" style={{ minHeight: '100vh', padding: '100px 48px' }}>
       <h2>About</h2>
-      <p>Hello, I'm Richard. I'm a Computer Science student based in Vancouver, BC. I enjoy building things for the web and am currently looking for internship opportunities.</p>
+      <p>Hello, I'm Richard. I am a fourth-year Computer Science student at Simon Fraser University. 
+        I have chosen the path of computer science, as I enjoy learning how the technology we use every day works and having the ability to build apps that 
+        I want at my disposal. Aside from computing and programming, I enjoy spending my time biking in nature and practicing yoyoing. </p>
     </div>
   )
 }
